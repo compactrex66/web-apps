27bca11ba75a7dc404cfc22ff626b8f356c41353
compactrex66 <compactrex66@gmail.com>
2025-12-08 15:04:36 +0100

    NWD finished

--- node/cw2/src/cw1.ts
+++ node/cw2/src/cw1.ts
@@ -8,21 +8,30 @@ const readObject = readLine.createInterface({
 
 const main = async () => {
     let number1, number2, nwdResult, choice;
-    number1 = parseInt(await readObject.question("Podaj pierwszą liczbę: "));
-    number2 = parseInt(await readObject.question("Podaj drugą liczbę: "));
-    choice = parseInt(await readObject.question("Podaj metodę jakiej chcesz użyć:\n1. Iteracyjnie\n2. Rekurencyjnie\n"));
-    readObject.close();
-    switch(choice) {
-        case 1:
-            nwdResult = await nwd(number1, number2);
-            break;
-        case 2:
-            nwdResult = await nwdRecursive(number1, number2);
-            break;
-        default:
-            console.log("Nie ma takiej opcji");
-            return
+    while(!number1) {
+        number1 = parseInt(await readObject.question("Podaj pierwszą liczbę: "));
     }
-    console.log(`Nwd liczb ${number1} i ${number2}: ${nwdResult}`);
+    while(!number2) {
+        number2 = parseInt(await readObject.question("Podaj drugą liczbę: "));
+    }
+    while(true) {
+        choice = parseInt(await readObject.question("Podaj metodę jakiej chcesz użyć:\n1. Iteracyjnie\n2. Rekurencyjnie\n"));
+        switch(choice) {
+            case 1:
+                nwdResult = await nwd(number1, number2);
+                break;
+            case 2:
+                nwdResult = await nwdRecursive(number1, number2);
+                break;
+            default:
+                console.log("Nie ma takiej opcji");
+                continue;
+        }
+        if(nwdResult) {
+            readObject.close();
+            console.log(`Nwd liczb ${number1} i ${number2}: ${nwdResult}`);
+            return;
+        }
+    }
 };
 main();
