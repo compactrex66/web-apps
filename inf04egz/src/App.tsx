9e079ddbc49f23ba677ef7ae03c125bedc14e18f
compactrex66 <compactrex66@gmail.com>
2026-05-19 10:51:22 +0200

    Finished colors exam

--- inf04egz/src/App.tsx
+++ inf04egz/src/App.tsx
@@ -8,24 +8,43 @@ function App() {
     const [blueValue, setBlueValue] = useState(255)
 
     const [color, setColor] = useState(`${redValue}, ${greenValue}, ${blueValue}`)
+    const [labelColor, setLabelColor] = useState(`${redValue}, ${greenValue}, ${blueValue}`)
 
     useEffect(() => {
         console.log(color)
         setColor(`${redValue}, ${greenValue}, ${blueValue}`);
     }, [redValue, greenValue, blueValue])
 
+    async function saveColorDB() {
+        setLabelColor(`${redValue}, ${greenValue}, ${blueValue}`)
+
+        const response = await fetch("http://localhost:5000/color", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                red: redValue,
+                green: greenValue,
+                blue: blueValue
+            })
+        })
+    }
+
     return (
-        <>
+        <div className='container'>
             <div className='color-rect' style={{
                 backgroundColor: `rgb(${color})`
             }}></div>
-            <span>Dobierz kolor suwakami i zapisz przyciskami</span>
+            <span className='left-aligned'>Dobierz kolor suwakami i zapisz przyciskami</span>
             <Slider setColorValue={setRedValue} letter='R'></Slider>
             <Slider setColorValue={setGreenValue} letter='G'></Slider>
             <Slider setColorValue={setBlueValue} letter='B'></Slider>
-            <button>Pobierz</button>
-            <div></div>
-        </>
+            <button onClick={saveColorDB}>Pobierz</button>
+            <div className='color-label' style={{
+                backgroundColor: `rgb(${labelColor})`
+            }}>{labelColor}</div>
+        </div>
     )
 }
 
--- inf04egz/src/components/Slider.tsx
+++ inf04egz/src/components/Slider.tsx
@@ -9,9 +9,9 @@ function Slider({setColorValue, letter}: SliderProps) {
     const [value, setValue] = useState(255);
 
     return (
-        <div>
+        <div className='slider-element'>
             <span>{letter}</span>
-            <input type="range" min="0" max="255" value={value} onChange={(e) => { 
+            <input className='slider' type="range" min="0" max="255" value={value} onChange={(e) => { 
                 setValue(Number(e.target.value));
                 setColorValue(e.target.value) 
             }}/>
